cf77587fae4f13f55fe2feab70c5f153fe7fcdeb
thabsheerjm <thabsheerjm1@gmail.com>
2025-06-23 22:37:07 -0400

    update train -  IoU

--- src/train.py
+++ src/train.py
@@ -3,14 +3,29 @@ from torch.utils.data import DataLoader, random_split
 from torchvision import models 
 from dataset import GOOSEDataset
 from tqdm import tqdm 
-from src.model import DeepLabHead
+from model import DeepLabHead
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 
 def train_config(config_path="src/config/config.json"):
     with open(config_path, 'r') as f:
         config = json.load(f)
         return config
+    
+def compute_metrics(preds, masks):
+    preds = (torch.sigmoid(preds)>0.5).int()
+    masks = masks.int()
+
+    intersection = (preds & masks).sum().item()
+    union = (preds | masks).sum().item()
+    correct = (preds == masks).sum().item()
+    total = preds.numel()
+
+    iou = intersection /union if union>0 else 1.0
+    accuracy = correct/total
+    return accuracy, iou 
 
 def train(config):
+    # Get configuration
     rgb_dir = config["data"]["rgb_dir"]
     mask_dir = config["data"]["mask_dir"]
     mapping_csv = config["data"]["mapping_csv"]
@@ -37,8 +52,12 @@ def train(config):
     optimizer = optim.Adam(model.parameters(), lr=lr)
 
     best_val_loss = float('inf')
+    best_val_iou = -float('inf')
 
-    
+    scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=4)
+    early_stop_patience = 10
+    epochs_since_improvement = 0 
+
     for epoch in range(epochs):
         model.train()
         running_loss = 0
@@ -57,6 +76,8 @@ def train(config):
         # Validation loop
         model.eval()
         val_loss = 0
+        val_accuracy = 0 
+        val_iou = 0 
         with torch.no_grad():
             for imgs, masks in tqdm(val_loader, desc="[Val]"):
                 imgs = imgs.cuda() if torch.cuda.is_available() else imgs
@@ -66,14 +87,34 @@ def train(config):
                 loss = criterion(outputs, masks)
                 val_loss += loss.item()
 
-        avg_val_loss = val_loss / len(val_loader)
-        print(f"Epoch {epoch+1}: Train Loss = {avg_train_loss:.4f}, Val Loss = {avg_val_loss:.4f}")
+                acc, iou =  compute_metrics(outputs, masks)
+                val_accuracy += acc
+                val_iou += iou 
 
-        # Save the best model
-        if avg_val_loss < best_val_loss:
-            best_val_loss = avg_val_loss
+        avg_val_loss = val_loss / len(val_loader)
+        avg_val_acc = val_accuracy/ len(val_loader)
+        avg_val_iou =  val_iou/ len(val_loader)
+
+
+        print(f"Epoch {epoch+1}: Train Loss = {avg_train_loss:.4f}, Val Loss = {avg_val_loss:.4f}, "
+                f"Val Acc = {avg_val_acc:.4f}, Val IoU = {avg_val_iou:.4f}")
+        
+        scheduler.step(avg_val_loss)
+
+        # Checkpointing
+        if avg_val_iou > best_val_iou:
+            best_val_iou = avg_val_iou
             torch.save(model.state_dict(), checkpoint_path)
-            print(f"Best model updated: Val Loss = {best_val_loss:.4f}")
+            print(f"Best model updated: Val IoU = {best_val_iou:.4f}")
+            epochs_since_improvement = 0
+        else:
+            epochs_since_improvement += 1
+            print(f"No improvement for {epochs_since_improvement} epoch(s)")
+
+        # Early stopping
+        if epochs_since_improvement >= early_stop_patience:
+            print("Early stopping triggered! Exiting.")
+            break
 
 if __name__ == '__main__':
     config = train_config()
