66d600b0419ea17a06ac8a80aaa2b4363d240ceb
thabsheerjm <thabsheerjm1@gmail.com>
2026-04-21 03:52:43 -0400

    Update reader - reft

--- backend/src/frame_reader.cpp
+++ backend/src/frame_reader.cpp
@@ -20,7 +20,14 @@ FrameReader::~FrameReader(){
 }
 
 bool FrameReader::next(cv::Mat& frame){
-    return cap_.read(frame) && !frame.empty();
+    // return cap_.read(frame) && !frame.empty();
+    // loop back to back
+    if (!cap_.read(frame) || frame.empty()){
+        cap_.set(cv::CAP_PROP_POS_FRAMES, 0);
+        cap_.read(frame);
+    }
+    return !frame.empty();
+
 }
 
 int FrameReader::width() const { return static_cast<int>(cap_.get(cv::CAP_PROP_FRAME_WIDTH)); }
